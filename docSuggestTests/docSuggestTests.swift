96b6b7787981ab0cb0fcfcf1cbdd712b962ea677
Deepak <deepak.bachulkar@doma.co.in>
2020-09-06 16:56:06 +0530

    Initial Commit

---
+++ docSuggestTests/docSuggestTests.swift
@@ -0,0 +1,34 @@
+//
+//  docSuggestTests.swift
+//  docSuggestTests
+//
+//  Created by Deepak Bachulkar on 06/09/20.
+//  Copyright © 2020 Deepak Bachulkar. All rights reserved.
+//
+
+import XCTest
+@testable import docSuggest
+
+class docSuggestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
